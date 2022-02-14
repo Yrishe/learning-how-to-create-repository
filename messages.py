b9cd71198bd1486dd7dbb9bdf932425c2fd4404f
Yarli Rabelo <y.matheus.borges@hotmail.com>
2022-02-14 10:41:29 +0000

    feat: adding new messages to project

--- messages.py
+++ messages.py
@@ -1,5 +1,6 @@
 display_messages = [
     'Seja feliz!',
     'Fique tranquilo, vai acabar tudo bem!',
-    'Acredite nos seus caminhos! Sempre!'
+    'Acredite nos seus caminhos! Sempre!',
+    'Vai na fé e com garra que você vai conseguir! com trabalho duro!'
 ]
