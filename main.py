8840b71997dff74562ac1f824c3fdc376076ca92
Yarli Rabelo <y.matheus.borges@hotmail.com>
2022-02-17 03:55:50 +0000

    feat: Implementing login

--- main.py
+++ main.py
@@ -7,7 +7,23 @@ import time
 print('starting project 3..2..1..')
 time.sleep(3)
 
+login_username = [
+    'Yarli',
+    'Matheus',
+    'Bianca'
+]
+
+username = input('usuario para login: ')
+
 while True:
+    
+    while (username not in login_username):
+        print()
+        print('Usuario Invalido! Tente novamente! ')
+        username = input('usuario para login: ')
+
+
+
     resposta = input('Deseja um conselho? S/N: ')
     if (resposta == 'S' or resposta == 's'):
         mensagem = random.choice(display_messages)
--- messages_test.py
+++ messages_test.py
@@ -2,4 +2,4 @@ from messages import display_messages
 
 def test_messages():
     assert 'Sempre apague as luzes antes de sair' in display_messages
-    
+     
