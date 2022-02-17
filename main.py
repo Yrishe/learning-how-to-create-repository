9d7b92d2e7d7c9dab80ae4b3cfad872f3eab3d7f
Yarli Rabelo <y.matheus.borges@hotmail.com>
2022-02-17 04:08:37 +0000

    feat:Implementing logout

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ while True:
         mensagem = random.choice(display_messages)
         print(mensagem)
         print()
-        
+    elif (resposta == 'N' or resposta == 'n'):   
+        exit() 
